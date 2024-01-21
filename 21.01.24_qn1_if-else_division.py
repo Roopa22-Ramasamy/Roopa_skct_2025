7d77323179bc2552ebd1213fcf65d247960f6549
Roopa22-Ramasamy <113413346+Roopa22-Ramasamy@users.noreply.github.com>
2024-01-21 19:27:10 +0530

    Update 21.01.24_qn1_if-else_division.py

--- 21.01.24_qn1_if-else_division.py
+++ 21.01.24_qn1_if-else_division.py
@@ -1,27 +1,27 @@
-1)	Question 
-Task
-Given an integer, , perform the following conditional actions:
-•	If  is odd, print Weird
-•	If  is even and in the inclusive range of  to , print Not Weird
-•	If  is even and in the inclusive range of  to , print Weird
-•	If  is even and greater than , print Not Weird
-Input Format
-A single line containing a positive integer, .
-Constraints
-•	
-Output Format
-Print Weird if the number is weird. Otherwise, print Not Weird.
-Sample Input 0
-3
-Sample Output 0
-Weird
-Explanation 0
+# 1)	Question 
+# Task
+# Given an integer, , perform the following conditional actions:
+# •	If  is odd, print Weird
+# •	If  is even and in the inclusive range of  to , print Not Weird
+# •	If  is even and in the inclusive range of  to , print Weird
+# •	If  is even and greater than , print Not Weird
+# Input Format
+# A single line containing a positive integer, .
+# Constraints
+# •	
+# Output Format
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+# Sample Input 0
+# 3
+# Sample Output 0
+# Weird
+# Explanation 0
 
- is odd and odd numbers are weird, so print Weird.
-Sample Input 1
-24
-Sample Output 1
-Not Weird
+#  is odd and odd numbers are weird, so print Weird.
+# Sample Input 1
+# 24
+# Sample Output 1
+# Not Weird
 
 solution :
 
@@ -47,33 +47,33 @@ if __name__ == '__main__':
             print("Not Weird")
 
 
-2) question
+# 2) question
 
-Task
-The provided code stub reads two integers,  and , from STDIN.
-Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
-No rounding or formatting is necessary.
-Example
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+# No rounding or formatting is necessary.
+# Example
 
 
-•	The result of the integer division .
-•	The result of the float division is .
-Print:
-0
-0.6
-Input Format
-The first line contains the first integer, .
-The second line contains the second integer, .
-Output Format
-Print the two lines as described above.
-Sample Input 0
-4
-3
-Sample Output 0
-1
-1.33333333333
+# •	The result of the integer division .
+# •	The result of the float division is .
+# Print:
+# 0
+# 0.6
+# Input Format
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+# Output Format
+# Print the two lines as described above.
+# Sample Input 0
+# 4
+# 3
+# Sample Output 0
+# 1
+# 1.33333333333
 
-solution :
+# solution :
 
 if __name__ == '__main__':
     a = int(input())
