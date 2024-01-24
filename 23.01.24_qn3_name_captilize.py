db03f1749eec3bc5f339a79794c403036e182ceb
Roopa22-Ramasamy <113413346+Roopa22-Ramasamy@users.noreply.github.com>
2024-01-24 09:47:38 +0530

    Update 23.01.24_qn3_name_captilize.py

--- 23.01.24_qn3_name_captilize.py
+++ 23.01.24_qn3_name_captilize.py
@@ -1,38 +1,38 @@
-1) You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# 1) You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
 
-Hello firstname lastname! You just delved into python.
+# Hello firstname lastname! You just delved into python.
 
-Function Description
+# Function Description
 
-Complete the print_full_name function in the editor below.
+# Complete the print_full_name function in the editor below.
 
-print_full_name has the following parameters:
+# print_full_name has the following parameters:
 
-string first: the first name
-string last: the last name
-Prints
+# string first: the first name
+# string last: the last name
+# Prints
 
-string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
-Input Format
+# string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
+# Input Format
 
-The first line contains the first name, and the second line contains the last name.
+# The first line contains the first name, and the second line contains the last name.
 
-Constraints
+# Constraints
 
-The length of the first and last names are each ≤ .
+# The length of the first and last names are each ≤ .
 
-Sample Input 0
+# Sample Input 0
 
-Ross
-Taylor
-Sample Output 0
+# Ross
+# Taylor
+# Sample Output 0
 
-Hello Ross Taylor! You just delved into python.
-Explanation 0
+# Hello Ross Taylor! You just delved into python.
+# Explanation 0
 
-The input read by the program is stored as a string data type. A string is a collection of characters.
+# The input read by the program is stored as a string data type. A string is a collection of characters.
 
-answer:
+# answer:
 
 
 def print_full_name(first, last):
@@ -40,30 +40,30 @@ def print_full_name(first, last):
 
 -------------------------------------------------------------------------------------------------------------------------
 
-2)You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+# 2)You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
 
 
-Given a full name, your task is to capitalize the name appropriately.
+# Given a full name, your task is to capitalize the name appropriately.
 
-Input Format
+# Input Format
 
-A single line of input containing the full name, .
+# A single line of input containing the full name, .
 
-Constraints
+# Constraints
 
-The string consists of alphanumeric characters and spaces.
-Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+# The string consists of alphanumeric characters and spaces.
+# Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
 
-Output Format
+# Output Format
 
-Print the capitalized string, .
+# Print the capitalized string, .
 
-Sample Input
+# Sample Input
 
-chris alan
-Sample Output
+# chris alan
+# Sample Output
 
-Chris Alan
+# Chris Alan
 
 
 solution:
